73885d5a9e4330d37c952ad42de4910a7cdf9e32
Yudha Pratama <yudha@vemobo.com>
2025-03-25 14:31:37 +0700

    partial fixed deployemnt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,11 @@
-module.exports = {
+export default {
   apps: [{
     name: 'pharmacy-frontend',
     script: '.output/server/index.mjs',
-    instances: 'max',
-    exec_mode: 'cluster',
+    instances: '1',
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      NUXT_PUBLIC_API_URL: 'http://your-server-ip:3001/api'
-    }
+    max_memory_restart: '1G'
   }]
 };
